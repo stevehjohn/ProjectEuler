0e45b2263de3cc6aa5b1d9e14059a8ef2f43e56c
Stevo <stevehuwjohn@gmail.com>
2024-02-17 16:50:42 +0000

    WIP

---
+++ src/ProjectEuler/Solutions/Puzzle0018.cs
@@ -0,0 +1,15 @@
+using JetBrains.Annotations;
+using ProjectEuler.Infrastructure;
+
+namespace ProjectEuler.Solutions;
+
+[UsedImplicitly]
+public class Puzzle0018 : Puzzle
+{
+    public override string GetAnswer()
+    {
+        LoadInput();
+        
+        throw new NotImplementedException();
+    }
+}
