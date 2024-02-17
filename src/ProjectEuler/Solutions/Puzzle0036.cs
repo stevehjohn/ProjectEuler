718f2b960d5324156de8a31d81098eb553d93989
Stevo <stevehuwjohn@gmail.com>
2024-02-17 19:30:33 +0000

    WIP

---
+++ src/ProjectEuler/Solutions/Puzzle0036.cs
@@ -0,0 +1,20 @@
+using JetBrains.Annotations;
+using ProjectEuler.Infrastructure;
+
+namespace ProjectEuler.Solutions;
+
+[UsedImplicitly]
+public class Puzzle0036 : Puzzle
+{
+    public override string GetAnswer()
+    {
+        var sum = 0;
+        
+        for (var i = 1; i < 1_000_000; i++)
+        {
+            
+        }
+
+        return sum.ToString("N0");
+    }
+}
