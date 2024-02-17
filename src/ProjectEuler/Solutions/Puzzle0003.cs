d78961b6f69c60120f551f76153ca6951f0c36f0
Stevo <stevehuwjohn@gmail.com>
2024-02-17 12:23:01 +0000

    WIP

--- src/ProjectEuler/Solutions/Puzzle0003.cs
+++ src/ProjectEuler/Solutions/Puzzle0003.cs
@@ -8,6 +8,29 @@ public class Puzzle0003 : Puzzle
 {
     public override string GetAnswer()
     {
-        return "?";
+        var number = 600_851_475_143;
+
+        var divisor = 2L;
+
+        while (number > 1)
+        {
+            if (number % divisor == 0)
+            {
+                number /= divisor;
+                
+                continue;
+            }
+
+            if (divisor > Math.Sqrt(number))
+            {
+                divisor = number;
+                
+                continue;
+            }
+
+            divisor++;
+        }
+
+        return divisor.ToString("N0");
     }
 }
