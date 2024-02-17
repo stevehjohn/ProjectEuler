e29a93f5df62c9a9a5fbb4401df2763740b936be
Stevo <stevehuwjohn@gmail.com>
2024-02-17 15:21:03 +0000

    Puzzle 0014

---
+++ src/ProjectEuler/Solutions/Puzzle0014.cs
@@ -0,0 +1,50 @@
+using JetBrains.Annotations;
+using ProjectEuler.Infrastructure;
+
+namespace ProjectEuler.Solutions;
+
+[UsedImplicitly]
+public class Puzzle0014 : Puzzle
+{
+    public override string GetAnswer()
+    {
+        var maxLength = 0;
+
+        var maxNumber = 0;
+        
+        for (var i = 1_000_000; i > 1; i--)
+        {
+            var length = GetSequenceLength(i);
+            
+            if (length > maxLength)
+            {
+                maxLength = length;
+
+                maxNumber = i;
+            }
+        }
+
+        return maxNumber.ToString("N0");
+    }
+
+    private static int GetSequenceLength(long number)
+    {
+        var length = 0;
+
+        while (number != 1)
+        {
+            if (number % 2 == 0)
+            {
+                number /= 2;
+            }
+            else
+            {
+                number = number * 3 + 1;
+            }
+
+            length++;
+        }
+
+        return length;
+    }
+}
