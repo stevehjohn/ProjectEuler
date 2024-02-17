edb27118ec9cfb7007b662818353e80d27200a0e
Stevo <stevehuwjohn@gmail.com>
2024-02-17 12:34:13 +0000

    Puzzle 0004

--- src/ProjectEuler/Solutions/Puzzle0004.cs
+++ src/ProjectEuler/Solutions/Puzzle0004.cs
@@ -8,6 +8,36 @@ public class Puzzle0004 : Puzzle
 {
     public override string GetAnswer()
     {
-        return "?";
+        var palindromes = new List<int>();
+        
+        for (var left = 999; left > 100; left--)
+        {
+            for (var right = 999; right > 100; right--)
+            {
+                var product = left * right;
+
+                if (IsPalindrome(product))
+                {
+                    palindromes.Add(product);
+                }
+            }
+        }
+
+        return palindromes.Max().ToString("N0");
+    }
+
+    private static bool IsPalindrome(int number)
+    {
+        var text = number.ToString();
+
+        for (var i = 0; i < text.Length / 2; i++)
+        {
+            if (text[i] != text[^(i + 1)])
+            {
+                return false;
+            }
+        }
+
+        return true;
     }
 }
