550adff591353e1330ad75a973ddf6eebb60ea13
Stevo <stevehuwjohn@gmail.com>
2024-02-17 13:35:41 +0000

    WIP

--- src/ProjectEuler/Solutions/Puzzle0009.cs
+++ src/ProjectEuler/Solutions/Puzzle0009.cs
@@ -8,6 +8,27 @@ public class Puzzle0009 : Puzzle
 {
     public override string GetAnswer()
     {
-        throw new NotImplementedException();
+        var a = 1L;
+
+        var b = 1L;
+        
+        while (true)
+        {
+            var c = a * a + b * b;
+
+            if (a + b + c == 1_000)
+            {
+                return (a * b * c).ToString("N0");
+            }
+
+            b++;
+
+            if (b > 1000)
+            {
+                a++;
+
+                b = a + 1;
+            }
+        }
     }
 }
