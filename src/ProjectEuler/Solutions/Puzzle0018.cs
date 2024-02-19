23d665abee98a5a2f22c1cf6d73ab08b14aa019f
Stevo <stevehuwjohn@gmail.com>
2024-02-19 18:18:13 +0000

    Puzzle 18

--- src/ProjectEuler/Solutions/Puzzle0018.cs
+++ src/ProjectEuler/Solutions/Puzzle0018.cs
@@ -10,6 +10,38 @@ public class Puzzle0018 : Puzzle
     {
         LoadInput();
 
-        return "?";
+        var queue = new Queue<(int Sum, int Index)>();
+
+        queue.Enqueue((ParseRow(0).Single(), 0));
+
+        var rowNumber = 1;
+        
+        var row = ParseRow(rowNumber);
+        
+        while (queue.TryDequeue(out var item))
+        {
+            queue.Enqueue((item.Sum + row[item.Index], item.Index));
+            
+            queue.Enqueue((item.Sum + row[item.Index + 1], item.Index + 1));
+            
+            if (item.Index + 2 == row.Count)
+            {
+                rowNumber++;
+
+                if (rowNumber == Input.Length)
+                {
+                    break;
+                }
+
+                row = ParseRow(rowNumber);
+            }
+        }
+
+        return queue.MaxBy(i => i.Sum).Sum.ToString("N0");
+    }
+    
+    private List<int> ParseRow(int rowNumber)
+    {
+        return Input[rowNumber].Split(' ').Select(int.Parse).ToList();
     }
 }
