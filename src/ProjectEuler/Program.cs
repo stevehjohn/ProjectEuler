f84f7e35ef662af3797a43f19926a1ec0918bb43
Stevo <stevehuwjohn@gmail.com>
2024-02-19 18:50:54 +0000

    wip

--- src/ProjectEuler/Program.cs
+++ src/ProjectEuler/Program.cs
@@ -32,7 +32,7 @@ public static class Program
         
         Console.WriteLine($"{new string(' ', 30)}--------------");
         
-        Console.WriteLine($"  {count} puzzles solved in {$"{elapsed / 1_000:N0}",22}ms");
+        Console.WriteLine($"  {count} puzzles solved in {$"{elapsed / 1_000:N0}",19}ms");
 
         Console.WriteLine();
     }
