5769fea40ea3ed30bf438451811689846fd244dd
Stevo <stevehuwjohn@gmail.com>
2024-02-19 18:24:26 +0000

    WIP

---
+++ src/ProjectEuler/Solutions/Puzzle0067.cs
@@ -0,0 +1,15 @@
+using JetBrains.Annotations;
+using ProjectEuler.Infrastructure;
+
+namespace ProjectEuler.Solutions;
+
+[UsedImplicitly]
+public class Puzzle0067 : Puzzle
+{
+    public override string GetAnswer()
+    {
+        LoadInput();
+
+        return "?";
+    }
+}
