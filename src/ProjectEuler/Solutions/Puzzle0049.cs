6d7fd3b01178e9ae3e0c4067c6784bf1d0a5e139
Stevo John <stevehuwjohn@gmail.com>
2024-02-20 12:07:44 +0000

    Puzzle 0049

--- src/ProjectEuler/Solutions/Puzzle0049.cs
+++ src/ProjectEuler/Solutions/Puzzle0049.cs
@@ -7,10 +7,48 @@ namespace ProjectEuler.Solutions;
 [UsedImplicitly]
 public class Puzzle0049 : Puzzle
 {
+    private const int Increment = 3_330;
+        
     public override string GetAnswer()
     {
         var primes = Maths.GetPrimes(10_000);
 
-        return "Unknown";
+        var hashes = primes.ToHashSet();
+
+        var result = 0L;
+        
+        foreach (var prime in primes)
+        {
+            if (prime == 1_487)
+            {
+                continue;
+            }
+
+            if (hashes.Contains(prime + Increment) && hashes.Contains(prime + Increment * 2))
+            {
+                if (IsPermutation(prime))
+                {
+                    result = long.Parse($"{prime}{prime + Increment}{prime + Increment * 2}");
+                }
+            }
+        }
+        
+        return result.ToString("N0");
+    }
+
+    private static bool IsPermutation(long number)
+    {
+        var left = new string(number.ToString().ToCharArray().OrderBy(c => c).ToArray());
+
+        var right = new string((number + Increment).ToString().ToCharArray().OrderBy(c => c).ToArray());
+
+        if (left != right)
+        {
+            return false;
+        }
+
+        right = new string((number + Increment * 2).ToString().ToCharArray().OrderBy(c => c).ToArray());
+
+        return left == right;
     }
 }
