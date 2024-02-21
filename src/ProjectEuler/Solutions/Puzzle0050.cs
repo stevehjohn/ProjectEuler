7d5d5c24d1b55014aecf26cdc4541a7f409ed23e
Stevo John <stevehuwjohn@gmail.com>
2024-02-21 09:17:21 +0000

    Puzzle 0050

--- src/ProjectEuler/Solutions/Puzzle0050.cs
+++ src/ProjectEuler/Solutions/Puzzle0050.cs
@@ -7,61 +7,38 @@ namespace ProjectEuler.Solutions;
 [UsedImplicitly]
 public class Puzzle0050 : Puzzle
 {
-    private List<long> _primes;
-
     public override string GetAnswer()
     {
-        _primes = Maths.GetPrimes(1_000_000);
+        var primes = Maths.GetPrimes(1_000_000);
 
-        var maxLength = 0;
-
-        var value = 0L;
-
-        foreach (var prime in _primes)
-        {
-            var length = GetSumLength(prime);
-
-            if (length > maxLength)
-            {
-                maxLength = length;
-
-                value = prime;
-            }
-        }
-
-        return value.ToString("N0");
-    }
-
-    private int GetSumLength(long prime)
-    {
-        var toTest = _primes.Where(p => p < prime / 2).ToList();
-
-        if (toTest.Count < 2)
-        {
-            return 0;
-        }
-
-        var sum = toTest.Sum();
-
-        var length = toTest.Count;
+        var hashes = primes.ToHashSet();
         
-        while (true)
+        var maxSum = 0L;
+
+        var length = 0;
+
+        for (var i = 0; i < primes.Count; i++)
         {
-            sum -= toTest.Last();
-            
-            toTest.RemoveAt(toTest.Count - 1);
+            var sum = 0L;
 
-            length--;
-
-            if (sum == prime)
+            for (var j = i; j < primes.Count; j++)
             {
-                return length;
-            }
+                sum += primes[j];
 
-            if (sum < prime || toTest.Count == 0)
-            {
-                return 0;
+                if (sum > 1_000_000)
+                {
+                    break;
+                }
+
+                if (hashes.Contains(sum) && sum > maxSum && j - i > length)
+                {
+                    length = j - i;
+
+                    maxSum = sum;
+                }
             }
         }
+
+        return maxSum.ToString("N0");
     }
 }
