27f979254d3ce15d221a6a5f34a56585523a7187
Stevo John <stevehuwjohn@gmail.com>
2024-03-05 09:08:42 +0000

    WIP

--- src/ProjectEuler/Solutions/Puzzle0096.cs
+++ src/ProjectEuler/Solutions/Puzzle0096.cs
@@ -1,3 +1,4 @@
+using System.Buffers;
 using System.Numerics;
 using JetBrains.Annotations;
 using ProjectEuler.Infrastructure;
@@ -7,6 +8,8 @@ namespace ProjectEuler.Solutions;
 [UsedImplicitly]
 public class Puzzle0096 : Puzzle
 {
+    private readonly ArrayPool<int> _pool = ArrayPool<int>.Shared;
+
     public override string GetAnswer()
     {
         LoadInput();
@@ -30,278 +33,176 @@ public class Puzzle0096 : Puzzle
         return sum.ToString("N0");
     }
 
-    private readonly int[] _rowCandidates = new int[9];
-    
-    private readonly int[] _columnCandidates = new int[9];
-    
-    private readonly int[] _boxCandidates = new int[9];
-
-    private readonly int[] _cellCandidates = new int[81];
-
-    private int[] Solve(int[] puzzle)
+    private int[] Solve(int[] sudoku)
     {
-        var workingCopy = new int[81];
-        
-        var score = 81;
-        
-        for (var i = 0; i < 81; i++)
-        {
-            if (puzzle[i] != 0)
-            {
-                score--;
+        var stack = new Stack<int[]>();
 
-                workingCopy[i] = puzzle[i];
+        stack.Push(sudoku);
+
+        var steps = 0;
+        
+        while (stack.TryPop(out var puzzle))
+        {
+            steps++;
+
+            var solutions = SolveStep(puzzle);
+
+            if (steps > 1)
+            {
+                _pool.Return(puzzle);
             }
-        }
 
-        var span = new Span<int>(workingCopy);
-        
-        GetCellCandidates(span);
-
-        SolveStep(span, score);
-        
-        return workingCopy;
-    }
-    
-    private bool SolveStep(Span<int> puzzle, int score)
-    {
-        FindHiddenSingles();
-        
-        var move = FindLowestMove(puzzle);
-
-        return CreateNextSteps(puzzle, move, score);
-    }
-
-    private void GetCellCandidates(Span<int> puzzle)
-    {
-        for (var y = 0; y < 9; y++)
-        {
-            _rowCandidates[y] = 0b11_1111_1111;
-
-            _columnCandidates[y] = 0b11_1111_1111;
-
-            var y9 = (y << 3) + y;
-
-            for (var x = 0; x < 9; x++)
+            foreach (var solution in solutions)
             {
-                _rowCandidates[y] &= ~(1 << puzzle[x + y9]);
-
-                _columnCandidates[y] &= ~(1 << puzzle[y + (x << 3) + x]);
-            }
-        }
-
-        var boxIndex = 0;
-        
-        for (var yO = 0; yO < 81; yO += 27)
-        {
-            for (var xO = 0; xO < 9; xO += 3)
-            {
-                var start = xO + yO;
-
-                _boxCandidates[boxIndex] = 0b11_1111_1111;
-
-                for (var y = 0; y < 3; y++)
+                if (solution.Solved)
                 {
-                    var row = start + (y << 3) + y;
-
-                    for (var x = 0; x < 3; x++)
+                    while (stack.TryPop(out puzzle))
                     {
-                        _boxCandidates[boxIndex] &= ~(1 << puzzle[row + x]);
+                        _pool.Return(puzzle);
                     }
+                    
+                    return solution.Sudoku;
                 }
 
-                boxIndex++;
+                stack.Push(solution.Sudoku);
             }
         }
+
+        return null;
+    }
+    
+    private List<(int[] Sudoku, bool Solved)> SolveStep(int[] sudoku)
+    {
+        var rowCandidates = new int[9];
+        
+        var columnCandidates = new int[9];
 
         for (var y = 0; y < 9; y++)
         {
+            rowCandidates[y] = 0b11_1111_1111;
+
+            columnCandidates[y] = 0b11_1111_1111;
+
+            var y9 = y * 9;
+            
             for (var x = 0; x < 9; x++)
             {
-                if (puzzle[x + (y << 3) + y] == 0)
-                {
-                    _cellCandidates[x + (y << 3) + y] = _columnCandidates[x] & _rowCandidates[y] & _boxCandidates[y / 3 * 3 + x / 3];
-                }
+                rowCandidates[y] &= ~(1 << sudoku[x + y9]);
+
+                columnCandidates[y] &= ~(1 << sudoku[y + x * 9]);
             }
         }
-    }
 
-    private void FindHiddenSingles()
-    {
-        for (var y = 0; y < 9; y++)
+        var boxCandidates = new int[9];
+
+        for (var y = 0; y < 9; y += 3) 
         {
-            var oneMaskRow = 0;
-        
-            var twoMaskRow = 0;
-        
-            var oneMaskColumn = 0;
-        
-            var twoMaskColumn = 0;
-        
-            for (var x = 0; x < 9; x++)
+            for (var x = 0; x < 3; x++)
             {
-                twoMaskRow |= oneMaskRow & _cellCandidates[(y << 3) + y + x];
-        
-                oneMaskRow |= _cellCandidates[(y << 3) + y + x];
+                boxCandidates[y + x] = 0b11_1111_1111;
 
-                twoMaskColumn |= oneMaskColumn & _cellCandidates[(x << 3) + x + y];
-        
-                oneMaskColumn |= _cellCandidates[(x << 3) + x + y];
-            }
-        
-            var onceRow = oneMaskRow & ~twoMaskRow;
-        
-            var onceColumn = oneMaskColumn & ~twoMaskColumn;
-        
-            if (BitOperations.PopCount((uint) onceRow) == 1)
-            {
-                for (var x = 0; x < 9; x++)
+                var x3 = x * 3;
+                
+                for (var y1 = 0; y1 < 3; y1++)
                 {
-                    if ((_cellCandidates[(y << 3) + y + x] & onceRow) > 0)
+                    var yy1 = y + y1;
+                    
+                    for (var x1 = 0; x1 < 3; x1++)
                     {
-                        _cellCandidates[(y << 3) + y + x] = onceRow;
-
-                        return;
-                    }
-                }
-            }
-        
-            if (BitOperations.PopCount((uint) onceColumn) == 1)
-            {
-                for (var x = 0; x < 9; x++)
-                {
-                    if ((_cellCandidates[(x << 3) + x + y] & onceColumn) > 0)
-                    {
-                        _cellCandidates[(x << 3) + x + y] = onceColumn;
-
-                        return;
+                        boxCandidates[y + x] &= ~(1 << sudoku[x3 + x1 + yy1 * 9]);
                     }
                 }
             }
         }
 
-        for (var yO = 0; yO < 81; yO += 27)
-        {
-            for (var xO = 0; xO < 9; xO += 3)
-            {
-                var oneMask = 0;
-
-                var twoMask = 0;
-
-                var start = yO + xO;
-
-                for (var y = 0; y < 3; y++)
-                {
-                    for (var x = 0; x < 3; x++)
-                    {
-                        twoMask |= oneMask & _cellCandidates[start + (y << 3) + y + x];
-
-                        oneMask |= _cellCandidates[start + (y << 3) + y + x];
-                    }
-                }
-
-                var once = oneMask & ~twoMask;
-
-                if (BitOperations.PopCount((uint) once) == 1)
-                {
-                    for (var y = 0; y < 3; y++)
-                    {
-                        for (var x = 0; x < 3; x++)
-                        {
-                            if ((_cellCandidates[start + (y << 3) + y + x] & once) > 0)
-                            {
-                                _cellCandidates[start + (y << 3) + y + x] = once;
-                            }
-                        }
-                    }
-                }
-            }
-        }
-    }
-    
-    private ((int X, int Y) Position, int Values, int ValueCount) FindLowestMove(Span<int> puzzle)
-    {
         var position = (X: -1, Y: -1);
 
         var values = 0;
 
         var valueCount = 0b11_1111_1111;
-
+        
         for (var y = 0; y < 9; y++)
         {
+            var row = rowCandidates[y];
+
+            if (row == 1)
+            {
+                continue;
+            }
+
+            var y9 = y * 9; 
+
             for (var x = 0; x < 9; x++)
             {
-                if (puzzle[x + (y << 3) + y] != 0)
+                if (sudoku[x + y9] != 0)
                 {
                     continue;
                 }
 
-                var candidates = _cellCandidates[x + (y << 3) + y];
-                
-                var count = BitOperations.PopCount((uint) candidates);
+                var column = columnCandidates[x];
 
+                var box = boxCandidates[y / 3 * 3 + x / 3];
+
+                var common = row & column & box;
+
+                if (common == 1)
+                {
+                    continue;
+                }
+
+                var count = BitOperations.PopCount((uint) common);
+                
                 if (count < valueCount)
                 {
                     position = (x, y);
 
-                    values = candidates;
+                    values = common;
 
                     valueCount = count;
                 }
             }
         }
 
-        return (position, values, valueCount);
-    }
+        var solutions = new List<(int[] Sudoku, bool Solved)>();
 
-    private bool CreateNextSteps(Span<int> puzzle, ((int X, int Y) Position, int Values, int ValueCount) move, int score)
-    {
         for (var i = 1; i < 10; i++)
         {
-            var bit = 1 << i;
-            
-            if ((move.Values & bit) == 0)
+            if ((values & (1 << i)) == 0)
             {
                 continue;
             }
 
-            puzzle[move.Position.X + (move.Position.Y << 3) + move.Position.Y] = i;
+            sudoku[position.X + position.Y * 9] = i;
 
-            var copy = new int[81];
-            
-            Array.Copy(_cellCandidates, copy, 81);
+            var copy = _pool.Rent(81);
 
-            var box = move.Position.Y / 3 * 27 + move.Position.X / 3 * 3;
-            
-            for (var j = 0; j < 9; j++)
+            var score = 81;
+        
+            for (var j = 0; j < 81; j++)
             {
-                _cellCandidates[j + move.Position.Y * 9] &= ~bit;
-                
-                _cellCandidates[move.Position.X + j * 9] &= ~bit;
-                
-                _cellCandidates[box + j % 3 + j / 3 * 9] &= ~bit;
-            }
+                var value = sudoku[j];
 
-            score--;
+                copy[j] = value;
+
+                if (value != 0)
+                {
+                    score--;
+                }
+            }
 
             if (score == 0)
             {
-                return true;
+                solutions.Clear();
+
+                solutions.Add((copy, true));
+                
+                break;
             }
-            
-            if (SolveStep(puzzle, score))
-            {
-                return true;
-            }
 
-            puzzle[move.Position.X + (move.Position.Y << 3) + move.Position.Y] = 0;
-
-            Array.Copy(copy, _cellCandidates, 81);
-
-            score++;
+            solutions.Add((copy, false));
         }
-
-        return false;
+        
+        return solutions;
     }
 
     private int[] LoadSudoku(int number)
