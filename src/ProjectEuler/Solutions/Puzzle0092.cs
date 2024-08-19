f8cd5c961a912f107d9d90006c3aec225566c79b
Steve John <steve.john@justeattakeaway.com>
2024-08-19 17:07:39 +0100

    WIP

--- src/ProjectEuler/Solutions/Puzzle0092.cs
+++ src/ProjectEuler/Solutions/Puzzle0092.cs
@@ -21,30 +21,28 @@ public class Puzzle0092 : Puzzle
         return count.ToString("N0");
     }
 
-    private bool ChainReturns89(int start)
+    private bool ChainReturns89(long value)
     {
-        var digits = start.ToString();
-        
         while (true)
         {
-            var sum = SumOfSquares(digits);
+            value = SumOfSquares(value);
             
-            if (sum == 1 || sum == 89)
+            if (value == 1 || value == 89)
             {
-                return sum == 89;
+                return value == 89;
             }
-
-            digits = sum.ToString();
         }
 }
 
-    private static long SumOfSquares(string digits)
+    private static long SumOfSquares(long digits)
     {
         var sum = 0L;
 
-        for (var i = 0; i < digits.Length; i++)
+        while (digits != 0)
         {
-            sum += (long) Math.Pow(digits[i] - '0', 2);
+            sum += digits % 10 * (digits % 10);
+
+            digits /= 10;
         }
 
         return sum;
