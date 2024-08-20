c7d0f91bfd4d86f3c6de476682f608a289e65d0a
Steve John <steve.john@justeattakeaway.com>
2024-08-20 11:13:16 +0100

    WIP

--- src/ProjectEuler/Solutions/Puzzle0023.cs
+++ src/ProjectEuler/Solutions/Puzzle0023.cs
@@ -5,31 +5,57 @@ namespace ProjectEuler.Solutions;
 
 public class Puzzle0023 : Puzzle
 {
+    private List<int> _abundant = new();
+    
     public override string GetAnswer()
     {
         var sum = 0;
         
         for (var i = 1; i < 28_123; i++)
         {
+            if (IsAbundant(i))
+            {
+                _abundant.Add(i);
+            }
+        }
+
+        for (var i = 1; i < 28_123; i++)
+        {
             Console.WriteLine(i);
             
-            if (i % 2 == 1)
+            if (! IsSumOfAbundant(i))
             {
                 sum += i;
-                
-                continue;
-            }
-
-            for (var j = 1; j < 28_123; j++)
-            {
-                if (! IsAbundant(i) && ! IsAbundant(j) && i + j < 28_123)
-                {
-                    sum += i + j;
-                }
             }
         }
 
         return sum.ToString("N0");
+    }
+
+    private bool IsSumOfAbundant(int number)
+    {
+        foreach (var l in _abundant)
+        {
+            if (l > number)
+            {
+                return false;
+            }
+
+            foreach (var r in _abundant)
+            {
+                if (r > number)
+                {
+                    break;
+                }
+
+                if (l + r == number)
+                {
+                    return true;
+                }
+            }
+        }
+
+        return false;
     }
 
     private static bool IsAbundant(int number)
