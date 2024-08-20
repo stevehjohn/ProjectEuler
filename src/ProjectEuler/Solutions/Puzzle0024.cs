8eee2f0689e582040f815ea6c2e6dd269996fd72
Steve John <steve.john@justeattakeaway.com>
2024-08-20 21:00:14 +0100

    WIP

---
+++ src/ProjectEuler/Solutions/Puzzle0024.cs
@@ -0,0 +1,33 @@
+using System.Text;
+using JetBrains.Annotations;
+using ProjectEuler.Infrastructure;
+
+namespace ProjectEuler.Solutions;
+
+[UsedImplicitly]
+public class Puzzle0024 : Puzzle
+{
+    private static readonly int[] Factorials = { 1, 1, 2, 6, 24, 120, 720, 5040, 40320, 362880 };
+    
+    public override string GetAnswer()
+    {
+        var digits = new List<int> { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+        var result = new StringBuilder();
+
+        var target = 1_000_000;
+        
+        for (var i = digits.Count - 1; i >= 0; i--)
+        {
+            var index = target / Factorials[i];
+
+            target %= Factorials[i];
+
+            result.Append(digits[index]);
+            
+            digits.RemoveAt(index);
+        }
+
+        return long.Parse(result.ToString()).ToString("N0");
+    }
+}
