c8d8fd5cb51d3891b366a03e1bc635f5fc04b5d9
Steve John <steve.john@justeattakeaway.com>
2024-08-20 16:00:54 +0100

    WIP

---
+++ src/ProjectEuler/Solutions/Puzzle0042.cs
@@ -0,0 +1,64 @@
+using JetBrains.Annotations;
+using ProjectEuler.Infrastructure;
+
+namespace ProjectEuler.Solutions;
+
+[UsedImplicitly]
+public class Puzzle0042 : Puzzle
+{
+    private string[] _words;
+
+    private HashSet<int> _triangles = new();
+    
+    public override string GetAnswer()
+    {
+        LoadWords();
+
+        GetTriangleNumbers(100);
+        
+        var count = 0;
+
+        foreach (var word in _words)
+        {
+            var value = GetWordValue(word);
+
+            if (_triangles.Contains(value))
+            {
+                count++;
+            }
+        }
+        
+        return count.ToString("N0");
+    }
+
+    private void GetTriangleNumbers(int max)
+    {
+        var n = 1;
+
+        while (n < max)
+        {
+            _triangles.Add((n * (n + 1)) / 2);
+
+            n++;
+        }
+    }
+
+    private static int GetWordValue(string word)
+    {
+        var value = 0;
+        
+        foreach (var letter in word)
+        {
+            value += letter - 'A' + 1;
+        }
+
+        return value;
+    }
+
+    private void LoadWords()
+    {
+        LoadInput();
+
+        _words = Input[0].Replace("\"", string.Empty).Split(',');
+    }
+}
