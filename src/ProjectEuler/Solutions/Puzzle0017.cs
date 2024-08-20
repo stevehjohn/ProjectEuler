84dbc98da53caa6b32c56f8176024ce8807fcae6
Steve John <steve.john@justeattakeaway.com>
2024-08-20 20:10:16 +0100

    WIP

---
+++ src/ProjectEuler/Solutions/Puzzle0017.cs
@@ -0,0 +1,25 @@
+using JetBrains.Annotations;
+using ProjectEuler.Extensions;
+using ProjectEuler.Infrastructure;
+
+namespace ProjectEuler.Solutions;
+
+[UsedImplicitly]
+public class Puzzle0017 : Puzzle
+{
+    private const int Max = 1_000;
+    
+    public override string GetAnswer()
+    {
+        var count = 0;
+        
+        for (var i = 1; i <= Max; i++)
+        {
+            count += i.NumberToEnglish().Length;
+            
+            Console.WriteLine(i.NumberToEnglish());
+        }
+
+        return count.ToString("N0");
+    }
+}
