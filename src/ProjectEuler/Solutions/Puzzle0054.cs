0fd73bcb722efc2cc5f11709f14f3cb3028b609b
Steve John <steve.john@justeattakeaway.com>
2024-08-21 14:41:16 +0100

    WIP

--- src/ProjectEuler/Solutions/Puzzle0054.cs
+++ src/ProjectEuler/Solutions/Puzzle0054.cs
@@ -11,14 +11,21 @@ public class Puzzle0054 : Puzzle
     {
         LoadInput();
 
+        var player1Wins = 0;
+        
         foreach (var line in Input)
         {
             var player1 = ParseHand(line[..14]);
 
             var player2 = ParseHand(line[15..]);
+
+            if (player1.Score() > player2.Score())
+            {
+                player1Wins++;
+            }
         }
-        
-        throw new NotImplementedException();
+
+        return player1Wins.ToString("N0");
     }
 
     private static Hand ParseHand(string hand)
@@ -31,10 +38,10 @@ public class Puzzle0054 : Puzzle
         {
             var suit = part[1] switch
             {
-                'C' => Suit.Clubs,
-                'D' => Suit.Diamonds,
-                'H' => Suit.Hearts,
-                'S' => Suit.Spades,
+                'C' => Suite.Clubs,
+                'D' => Suite.Diamonds,
+                'H' => Suite.Hearts,
+                'S' => Suite.Spades,
                 _ => throw new PuzzleException($"Unrecognised suite {part[1]}.")
             };
 
@@ -57,15 +64,74 @@ public class Puzzle0054 : Puzzle
 
     private class Hand
     {
-        private IReadOnlyList<Card> Cards { get; init; }
+        private readonly Card[] _cards;
 
         public Hand(List<Card> cards)
         {
-            Cards = cards;
+            _cards = cards.OrderBy(c => c.Value).ToArray();
+        }
+
+        public int Score()
+        {
+            if (ConsecutiveSameSuite())
+            {
+                if (_cards[0].Value == Value.Ten)
+                {
+                    return 900;
+                }
+
+                return 800 + (int) _cards[4].Value;
+            }
+
+            var matchCounts = CountMatches();
+
+            if (matchCounts[0] == 4)
+            {
+                return 700 + 20 * (int) _cards[0].Value + (int) _cards[4].Value;
+            }
+
+            if (matchCounts[1] == 4)
+            {
+                return 700 + 20 * (int) _cards[1].Value + (int) _cards[0].Value;
+            }
+
+            return 0;
+        }
+
+        private List<int> CountMatches()
+        {
+            var matchCounts = new Dictionary<Value, int>();
+
+            foreach (var card in _cards)
+            {
+                if (! matchCounts.TryAdd(card.Value, 1))
+                {
+                    matchCounts[card.Value]++;
+                }
+            }
+
+            return matchCounts.Select(c => c.Value).ToList();
+        }
+
+        private bool ConsecutiveSameSuite()
+        {
+            var current = _cards[0];
+            
+            for (var i = 1; i < 5; i++)
+            {
+                if (_cards[i].Value != current.Value + 1 || _cards[i].Suite != current.Suite)
+                {
+                    return false;
+                }
+
+                current = _cards[i];
+            }
+
+            return true;
         }
     }
 
-    private enum Suit
+    private enum Suite
     {
         Clubs,
         Hearts,
@@ -75,6 +141,7 @@ public class Puzzle0054 : Puzzle
 
     private enum Value
     {
+        // ReSharper disable UnusedMember.Local
         Two,
         Three,
         Four,
@@ -83,6 +150,7 @@ public class Puzzle0054 : Puzzle
         Seven,
         Eight,
         Nine,
+        //  ReSharper restore UnusedMember.Local
         Ten,
         Jack,
         Queen,
@@ -92,13 +160,13 @@ public class Puzzle0054 : Puzzle
 
     private class Card
     {
-        public Suit Suit { get; init; }
+        public Suite Suite { get; init; }
 
         public Value Value { get; init; }
 
-        public Card(Suit suit, Value value)
+        public Card(Suite suit, Value value)
         {
-            Suit = suit;
+            Suite = suit;
             
             Value = value;
         }
