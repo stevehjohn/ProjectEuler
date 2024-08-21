f870abfbbbc0543b39c8b96a8c8be781f9fef09d
Steve John <steve.john@justeattakeaway.com>
2024-08-21 18:13:57 +0100

    53

--- src/ProjectEuler/Solutions/Puzzle0053.cs
+++ src/ProjectEuler/Solutions/Puzzle0053.cs
@@ -1,5 +1,6 @@
 using JetBrains.Annotations;
 using ProjectEuler.Infrastructure;
+using ProjectEuler.Libraries;
 
 namespace ProjectEuler.Solutions;
 
@@ -8,6 +9,21 @@ public class Puzzle0053 : Puzzle
 {
     public override string GetAnswer()
     {
+        var count = 0;
         
+        for (var n = 2; n <= 100; n++)
+        {
+            for (var r = 1; r < n; r++)
+            {
+                var combinations = Maths.Factorial(n) / (Maths.Factorial(r) * Maths.Factorial(n - r));
+
+                if (combinations > 1_000_000)
+                {
+                    count++;
+                }
+            }
+        }
+
+        return count.ToString("N0");
     }
 }
