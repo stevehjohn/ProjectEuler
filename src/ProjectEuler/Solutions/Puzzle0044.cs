4b749d3413fd1b6553dda71c482fecc66774ac3e
Steve John <steve.john@justeattakeaway.com>
2024-08-21 12:08:17 +0100

    44 WIP

---
+++ src/ProjectEuler/Solutions/Puzzle0044.cs
@@ -0,0 +1,68 @@
+using JetBrains.Annotations;
+using ProjectEuler.Infrastructure;
+
+namespace ProjectEuler.Solutions;
+
+[UsedImplicitly]
+public class Puzzle0044 : Puzzle
+{
+    private List<int> _pentagonals;
+    
+    public override string GetAnswer()
+    {
+        GeneratePentagonals();
+
+        var minimumDifference = int.MaxValue;
+        
+        for (var l = 0; l < _pentagonals.Count; l++)
+        {
+            for (var r = l + 1; r < _pentagonals.Count; r++)
+            {
+                if (IsPentagonal(_pentagonals[l] + _pentagonals[r]))
+                {
+                    var difference = _pentagonals[l] - _pentagonals[r];
+
+                    if (IsPentagonal(difference) && difference < minimumDifference)
+                    {
+                        minimumDifference = difference;
+                    }
+                }
+            }
+        }
+
+        return minimumDifference.ToString("N0");
+    }
+
+    private void GeneratePentagonals()
+    {
+        _pentagonals = new List<int> { 1 };
+
+        var i = 2;
+        
+        while (true)
+        {
+            var next = i * (3 * i - 1) / 2;
+            
+            _pentagonals.Add(next);
+
+            if (next > 10_000_000)
+            {
+                break;
+            }
+
+            i++;
+        }
+    }
+    
+    private static bool IsPentagonal(long number)
+    {
+        var discriminant = 1d + 24 * number;
+
+        var rootDiscriminant = Math.Sqrt(discriminant);
+
+        var k = (1 + rootDiscriminant) / 6;
+
+        // ReSharper disable once CompareOfFloatsByEqualityOperator
+        return k == (int) k;
+    }
+}
