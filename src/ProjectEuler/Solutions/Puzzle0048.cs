b4236b5836d4864a4129e235fae1f4bf50c695ea
Steve John <steve.john@justeattakeaway.com>
2024-08-21 09:12:12 +0100

    WIP

---
+++ src/ProjectEuler/Solutions/Puzzle0048.cs
@@ -0,0 +1,38 @@
+using JetBrains.Annotations;
+using ProjectEuler.Infrastructure;
+
+namespace ProjectEuler.Solutions;
+
+[UsedImplicitly]
+public class Puzzle0048 : Puzzle
+{
+    public override string GetAnswer()
+    {
+        var result = 0L;
+
+        for (var i = 1; i <= 1_000; i++)
+        {
+            result += ModPow(i, i, 10_000_000_000);
+        }
+
+        return result.ToString("N0");
+    }
+
+    private long ModPow(long b, long e, long m)
+    {
+        var r = 1L;
+
+        while (e > 0)
+        {
+            if ((e & 1) == 1)
+            {
+                r = r * b % m;
+            }
+
+            b = (b * b) % m;
+            e >>= 1;
+        }
+
+        return r;
+    }
+}
