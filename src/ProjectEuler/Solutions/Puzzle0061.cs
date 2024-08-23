9b96217900de2ae81d89e21e197e042476c533e2
Steve John <steve.john@justeattakeaway.com>
2024-08-23 17:42:34 +0100

    WIP

--- src/ProjectEuler/Solutions/Puzzle0061.cs
+++ src/ProjectEuler/Solutions/Puzzle0061.cs
@@ -7,13 +7,75 @@ namespace ProjectEuler.Solutions;
 [UsedImplicitly]
 public class Puzzle0061 : Puzzle
 {
-    private readonly Dictionary<NumberType, List<long>> _numbers = [];
+    private readonly Dictionary<NumberType, HashSet<long>> _numbers = [];
     
     public override string GetAnswer()
     {
         GenerateShapedNumbers();
         
-        throw new NotImplementedException();
+        foreach (var oct in _numbers[NumberType.Octagonal])
+        {
+            var chain = WalkChain([(oct, NumberType.Octagonal)]);
+
+            if (chain != null)
+            {
+            }
+        }
+
+        return "0";
+    }
+    
+    private List<(long Number, NumberType Shape)> WalkChain(List<(long Number, NumberType Shape)> chain)
+    {
+        if (chain.Count >= 5)
+        {
+            Console.WriteLine(string.Join(" ", chain.Select(c => $"{c.Number:0000} {c.Shape.ToString()[..3]}")));
+        }
+
+        if (chain.Count == 6)
+        {
+            return chain;
+        }
+
+        if (chain.All(i => i.Shape != NumberType.Heptagonal))
+        {
+            WalkChain(chain, NumberType.Heptagonal);
+        }
+
+        if (chain.All(i => i.Shape != NumberType.Hexagonal))
+        {
+            WalkChain(chain, NumberType.Hexagonal);
+        }
+
+        if (chain.All(i => i.Shape != NumberType.Pentagonal))
+        {
+            WalkChain(chain, NumberType.Pentagonal);
+        }
+
+        if (chain.All(i => i.Shape != NumberType.Square))
+        {
+            WalkChain(chain, NumberType.Square);
+        }
+
+        if (chain.All(i => i.Shape != NumberType.Triangle))
+        {
+            WalkChain(chain, NumberType.Triangle);
+        }
+        
+        return null;
+    }
+
+    private void WalkChain(List<(long Number, NumberType Shape)> chain, NumberType shape)
+    {
+        var end = chain.Last().Number % 100;
+        
+        foreach (var number in _numbers[shape])
+        {
+            if (number / 100 == end)
+            {
+                WalkChain([..chain, (number, shape)]);
+            }
+        }
     }
 
     private void GenerateShapedNumbers()
@@ -41,7 +103,7 @@ public class Puzzle0061 : Puzzle
             var number = Maths.GenerateShapedNumber(origin, shape);
 
             origin++;
-
+            
             if (number < 1_000)
             {
                 continue;
