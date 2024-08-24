48c838fdc273aa873643d9e7968602088b5d47d5
Stevo John <stevehuwjohn@gmail.com>
2024-08-24 16:09:27 +0100

    173

---
+++ src/ProjectEuler/Solutions/Puzzle0173.cs
@@ -0,0 +1,74 @@
+using JetBrains.Annotations;
+using ProjectEuler.Infrastructure;
+
+namespace ProjectEuler.Solutions;
+
+[UsedImplicitly]
+public class Puzzle0173 : Puzzle
+{
+    private const int MaxTiles = 1000000;
+    
+    public override string GetAnswer()
+    {
+        var count = 0;
+
+        for (var holeSize = 1; holeSize < MaxTiles; holeSize++)
+        {
+            var complete = false;
+            
+            for (var layers = 1; layers < MaxTiles; layers++)
+            {
+                var tiles = GetTileCount(holeSize, layers);
+                
+                if (tiles == 0)
+                {
+                    if (layers == 1)
+                    {
+                        complete = true;
+                    }
+                    
+                    break;
+                }
+
+                count++;
+            }
+
+            if (complete)
+            {
+                break;
+            }
+        }
+
+        return count.ToString("N0");
+    }
+
+    private static int GetTileCount(int holeSize, int layers)
+    {
+        var totalTiles = 0;
+        
+        var sideSize = holeSize + 1;
+
+        while (layers > 0)
+        {
+            var tiles = sideSize * 4;
+
+            if (totalTiles + tiles <= MaxTiles)
+            {
+                totalTiles += tiles;
+
+                layers--;
+
+                sideSize += 2;
+            }
+            else
+            {
+                totalTiles = 0;
+                
+                break;
+            }
+
+        }
+        
+        return totalTiles;
+    }
+}
