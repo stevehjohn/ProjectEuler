bf0eab692a2e8b0848b54d515b97585f119f03d4
Steve John <steve.john@justeattakeaway.com>
2024-08-24 11:12:43 +0100

    WIP

---
+++ src/ProjectEuler/Solutions/Puzzle0197.cs
@@ -0,0 +1,13 @@
+using JetBrains.Annotations;
+using ProjectEuler.Infrastructure;
+
+namespace ProjectEuler.Solutions;
+
+[UsedImplicitly]
+public class Puzzle0197 : Puzzle
+{
+    public override string GetAnswer()
+    {
+        return "TODO";
+    }
+}
