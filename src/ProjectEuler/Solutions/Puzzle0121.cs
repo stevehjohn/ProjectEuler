5e7e105a096f741b1e4bcf34d2d99f6b1bd5b8c7
Stevo John <stevehuwjohn@gmail.com>
2024-08-24 19:22:02 +0100

    121

--- src/ProjectEuler/Solutions/Puzzle0121.cs
+++ src/ProjectEuler/Solutions/Puzzle0121.cs
@@ -1,48 +1,65 @@
 using System.Numerics;
 using JetBrains.Annotations;
 using ProjectEuler.Infrastructure;
+using ProjectEuler.Libraries;
 
 namespace ProjectEuler.Solutions;
 
 [UsedImplicitly]
 public class Puzzle0121 : Puzzle
 {
-    private const int MaxRounds = 4;
+    private const int MaxRounds = 63;
     
     public override string GetAnswer()
     {
         var wins = GenerateWins();
 
+        var denominator = Maths.Factorial(MaxRounds + 1);
+
+        var numerator = 0L;
+        
         foreach (var win in wins)
         {
-            Console.WriteLine($"{win:b8}");
+            numerator += GetNumeratorForRound(win);
         }
-        
-        return "0";
+
+        // ReSharper disable once IntDivisionByZero
+        return (denominator / numerator).ToString("N0");
     }
 
-    private List<uint> GenerateWins()
+    private static long GetNumeratorForRound(ulong round)
+    {
+        var numerator = 1;
+        
+        for (var i = 0; i < MaxRounds; i++)
+        {
+            if ((round & (1ul << i)) == 0)
+            {
+                numerator *= i + 1;
+            }
+        }
+
+        return numerator;
+    }
+
+    private static IEnumerable<ulong> GenerateWins()
     {
         var i = 1;
 
         var target = MaxRounds / 2 + 1;
 
-        var wins = new List<uint>();
-
         var grayCode = 0u;
         
-        while ((grayCode & (1 << MaxRounds)) == 0)
+        while ((grayCode & (1ul << MaxRounds)) == 0)
         {
             grayCode = (uint) (i ^ (i >> 1)); 
             
             if (BitOperations.PopCount(grayCode) >= target)
             {
-                wins.Add(grayCode);
+                yield return grayCode;
             }
 
             i++;
         }
-
-        return wins;
     }
 }
